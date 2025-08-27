e2ac42fdfaccc2074372a391b273c67ca284d04c
Junchao Ma <junchao1997@hotmail.com>
2025-08-26 21:37:26 -0700

    Updated minipi model and config

---
+++ source/mini_pi/hightorque.py
@@ -0,0 +1,150 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Configuration for Hightorque robots.
+
+The following configurations are available:
+
+* :obj:`Pai_MINIMAL_CFG`: Pai biped robot with minimal collision bodies
+
+Reference: https://github.com/hightorquerobotics/hightorque_ros
+"""
+
+import os
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+
+ASSETS_DIR = os.environ.get("SIM_ASSETS_PATH")
+
+MPCL = [
+    [-1.25, 1.75],  # l_hip_pitch_joint
+    [-0.12, 0.5],   # l_hip_roll_joint
+    [-0.3, 0.6],    # l_thigh_joint
+    [-0.65, 1.65],  # l_calf_joint
+    [-0.5, 1.3],    # l_ankle_pitch_joint
+    [-0.15, 0.15],  # l_ankle_roll_joint
+    [-1.25, 1.75],  # r_hip_pitch_joint
+    [-0.5, 0.12],   # r_hip_roll_joint
+    [-0.6, 0.3],    # r_thigh_joint
+    [-0.65, 1.65],  # r_calf_joint
+    [-0.5, 1.3],    # r_ankle_pitch_joint
+    [-0.15, 0.15],  # r_ankle_roll_joint
+]
+
+PAI_CFG = ArticulationCfg(
+    spawn=sim_utils.UrdfFileCfg(
+        asset_path=os.path.join(ASSETS_DIR, "mini_pi/pi_12dof_release_v1.urdf"),
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        fix_base=True,
+        joint_drive=sim_utils.UrdfConverterCfg.JointDriveCfg(
+            drive_type="force",
+            target_type="position",
+            gains=sim_utils.UrdfConverterCfg.JointDriveCfg.PDGainsCfg(
+                stiffness={
+                    ".*_hip_pitch_joint": 20.0,
+                    ".*_hip_roll_joint": 20.0,
+                    ".*_thigh_joint": 20.0,
+                    ".*_calf_joint": 20.0,
+                    ".*_ankle_pitch_joint": 20.0,
+                    ".*_ankle_roll_joint": 20.0,
+                },
+                damping={
+                    ".*_hip_pitch_joint": 0.5,
+                    ".*_hip_roll_joint": 0.5,
+                    ".*_thigh_joint": 0.5,
+                    ".*_calf_joint": 0.5,
+                    ".*_ankle_pitch_joint": 0.5,
+                    ".*_ankle_roll_joint": 0.5,
+                },
+            ),
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=1,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.3453),
+        joint_pos={
+            ".*_hip_pitch_joint": 0.0,
+            ".*_hip_roll_joint": 0.0,
+            ".*_thigh_joint": 0.0,
+            ".*_calf_joint": 0.0,
+            ".*_ankle_pitch_joint": 0.0,
+            ".*_ankle_roll_joint": 0.0,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.90,
+    actuators={
+        "legs": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_hip_pitch_joint",
+                ".*_hip_roll_joint",
+                ".*_thigh_joint",
+                ".*_calf_joint",
+            ],
+            effort_limit_sim={
+                ".*_hip_pitch_joint": 20.0,
+                ".*_hip_roll_joint": 20.0,
+                ".*_thigh_joint": 20.0,
+                ".*_calf_joint": 20.0,
+            },
+            velocity_limit_sim={
+                ".*_hip_pitch_joint": 21.0,
+                ".*_hip_roll_joint": 21.0,
+                ".*_thigh_joint": 21.0,
+                ".*_calf_joint": 21.0,
+            },
+            stiffness={
+                ".*_hip_pitch_joint": 20.0,
+                ".*_hip_roll_joint": 20.0,
+                ".*_thigh_joint": 20.0,
+                ".*_calf_joint": 20.0,
+            },
+            damping={
+                ".*_hip_pitch_joint": 0.5,
+                ".*_hip_roll_joint": 0.5,
+                ".*_thigh_joint": 0.5,
+                ".*_calf_joint": 0.5,
+            },
+            armature=0.01,
+        ),
+        "feet": ImplicitActuatorCfg(
+            joint_names_expr=[
+                ".*_ankle_pitch_joint",
+                ".*_ankle_roll_joint",
+            ],
+            effort_limit_sim={
+                ".*_ankle_pitch_joint": 20.0,
+                ".*_ankle_roll_joint": 20.0,
+            },
+            velocity_limit_sim={
+                ".*_ankle_pitch_joint": 21.1,
+                ".*_ankle_roll_joint": 21.1,
+            },
+            stiffness={
+                ".*_ankle_pitch_joint": 20.0,
+                ".*_ankle_roll_joint": 20.0,
+            },
+            damping={
+                ".*_ankle_pitch_joint": 0.5,
+                ".*_ankle_roll_joint": 0.5,
+            },
+            armature=0.01,
+        ),
+    },
+)
