ab0860136b5c94f8591cf5be27a90042e072f62a
Junchao Ma <junchao1997@hotmail.com>
2025-08-27 17:35:12 -0700

    changed configuration file to standard format, tested

--- source/hector_mini/hector_mini.py
+++ source/hector_mini/hector_mini.py
@@ -6,6 +6,36 @@ from isaaclab.actuators import ImplicitActuatorCfg
 ##
 # Configuration
 ##
+JOINT_NAME_EXPR = [
+    "left_hip_yaw_joint",
+    "right_hip_yaw_joint",
+    "left_hip_roll_joint",
+    "right_hip_roll_joint",
+    "left_hip_pitch_joint",
+    "right_hip_pitch_joint",
+    "left_knee_joint",
+    "right_knee_joint",
+    "left_calf_joint",
+    "right_calf_joint",
+    "left_ankle_joint",
+    "right_ankle_joint",
+]
+
+DEFAULT_JOINT_POS = {
+    "left_hip_yaw_joint": 0.0,  # 0
+    "right_hip_yaw_joint": 0.0,  # 6
+    "left_hip_roll_joint": 0.0,  # 1
+    "right_hip_roll_joint": 0.0,  # 7
+    "left_hip_pitch_joint": -0.4,  # 2
+    "right_hip_pitch_joint": -0.4,  # 8
+    "left_knee_joint": 0.5,  # 3
+    "right_knee_joint": 0.5,  # 14
+    "left_calf_joint": 0.1,  # 4
+    "right_calf_joint": 0.1,  # 9
+    "left_ankle_joint": -0.3,  # 5
+    "right_ankle_joint": -0.3,  # 10
+}
+
 STIFFNESS = {
     "left_hip_yaw_joint": 10.0,
     "right_hip_yaw_joint": 10.0,
@@ -36,6 +66,36 @@ DAMPING = {
     "right_ankle_joint": 1.0,
 }
 
+EFFORT_LIMIT = {
+    "left_hip_yaw_joint": 17.0,
+    "right_hip_yaw_joint": 17.0,
+    "left_hip_roll_joint": 17.0,
+    "right_hip_roll_joint": 17.0,
+    "left_hip_pitch_joint": 17.0,
+    "right_hip_pitch_joint": 17.0,
+    "left_knee_joint": 17.0,
+    "right_knee_joint": 17.0,
+    "left_calf_joint": 17.0,
+    "right_calf_joint": 17.0,
+    "left_ankle_joint": 17.0,
+    "right_ankle_joint": 17.0,
+}
+
+VELOCITY_LIMIT = {
+    "left_hip_yaw_joint": 22.0,
+    "right_hip_yaw_joint": 22.0,
+    "left_hip_roll_joint": 22.0,
+    "right_hip_roll_joint": 22.0,
+    "left_hip_pitch_joint": 22.0,
+    "right_hip_pitch_joint": 22.0,
+    "left_knee_joint": 22.0,
+    "right_knee_joint": 22.0,
+    "left_calf_joint": 22.0,
+    "right_calf_joint": 22.0,
+    "left_ankle_joint": 22.0,
+    "right_ankle_joint": 22.0,
+}
+
 import os
 
 ASSETS_DIR = os.environ.get("SIM_ASSETS_PATH")
@@ -55,7 +115,6 @@ MPCL = [
     [-2.0, 2.0],  # r_ankle_joint
 ]
 
-print(ASSETS_DIR)
 # implicit actuator
 HECTOR_MINI_CFG = ArticulationCfg(
     spawn=sim_utils.UrdfFileCfg(
@@ -83,66 +142,14 @@ HECTOR_MINI_CFG = ArticulationCfg(
     init_state=ArticulationCfg.InitialStateCfg(
         pos=(0.0, 0.0, 0.65),  # when fix_root_link=False
         # pos=(0.0, 0.0, 1.00), # when fix_root_link=True
-        joint_pos={
-            "left_hip_yaw_joint": 0.0,  # 0
-            "right_hip_yaw_joint": 0.0,  # 6
-            "left_hip_roll_joint": 0.0,  # 1
-            "right_hip_roll_joint": 0.0,  # 7
-            "left_hip_pitch_joint": -0.4,  # 2
-            "right_hip_pitch_joint": -0.4,  # 8
-            "left_knee_joint": 0.5,  # 3
-            "right_knee_joint": 0.5,  # 14
-            "left_calf_joint": 0.1,  # 4
-            "right_calf_joint": 0.1,  # 9
-            "left_ankle_joint": -0.3,  # 5
-            "right_ankle_joint": -0.3,  # 10
-        },
+        joint_pos=DEFAULT_JOINT_POS,
         joint_vel={".*": 0.0},
     ),
     actuators={
         "limbs": ImplicitActuatorCfg(
-            joint_names_expr=[
-                "left_hip_yaw_joint",
-                "right_hip_yaw_joint",
-                "left_hip_roll_joint",
-                "right_hip_roll_joint",
-                "left_hip_pitch_joint",
-                "right_hip_pitch_joint",
-                "left_knee_joint",
-                "right_knee_joint",
-                "left_calf_joint",
-                "right_calf_joint",
-                "left_ankle_joint",
-                "right_ankle_joint",
-            ],
-            effort_limit={
-                "left_hip_yaw_joint": 17.0,
-                "right_hip_yaw_joint": 17.0,
-                "left_hip_roll_joint": 17.0,
-                "right_hip_roll_joint": 17.0,
-                "left_hip_pitch_joint": 17.0,
-                "right_hip_pitch_joint": 17.0,
-                "left_knee_joint": 17.0,
-                "right_knee_joint": 17.0,
-                "left_calf_joint": 17.0,
-                "right_calf_joint": 17.0,
-                "left_ankle_joint": 17.0,
-                "right_ankle_joint": 17.0,
-            },
-            velocity_limit={
-                "left_hip_yaw_joint": 22.0,
-                "right_hip_yaw_joint": 22.0,
-                "left_hip_roll_joint": 22.0,
-                "right_hip_roll_joint": 22.0,
-                "left_hip_pitch_joint": 22.0,
-                "right_hip_pitch_joint": 22.0,
-                "left_knee_joint": 22.0,
-                "right_knee_joint": 22.0,
-                "left_calf_joint": 22.0,
-                "right_calf_joint": 22.0,
-                "left_ankle_joint": 22.0,
-                "right_ankle_joint": 22.0,
-            },
+            joint_names_expr=JOINT_NAME_EXPR,
+            effort_limit=EFFORT_LIMIT,
+            velocity_limit=VELOCITY_LIMIT,
             stiffness=STIFFNESS,
             damping=DAMPING,
         ),
