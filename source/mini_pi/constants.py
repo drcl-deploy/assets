0437c3bc556168a1ada3662a133e4bd7bfbea2a9
Junchao Ma <junchao1997@hotmail.com>
2025-09-12 11:06:08 -0700

    Added tracked ee links constant and foot sampling range

--- source/mini_pi/constants.py
+++ source/mini_pi/constants.py
@@ -22,3 +22,23 @@ BAD_CONTACT_BODIES = [
 
 
 ROOT_LINK_NAME = "base_link"
+TRACKED_EE_LINKS = [
+    {
+        "name": "l_foot",
+        "body_name": "l_ankle_roll_link",
+        "cmd_attr": "cmd_left_foot_pos",
+    },
+    {
+        "name": "r_foot",
+        "body_name": "r_ankle_roll_link",
+        "cmd_attr": "cmd_right_foot_pos",
+    },
+]
+
+SAMPLING_RANGE = {
+    "left_foot_pos": [
+        [-0.0, 0.15, 0.01],
+        [0.0, 0.15, 0.14],
+    ],  # Lower limit, upper limit
+    "right_foot_pos": [[0.0, -0.15, 0.01], [0.0, -0.15, 0.01]],
+}
