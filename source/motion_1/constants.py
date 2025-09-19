9c06acb5a007c3aa198c9656aa8f877ea73b15da
lok-i <lokesha1b2c3@gmail.com>
2025-09-19 16:24:37 -0700

    ee added for g1, motion_1

--- source/motion_1/constants.py
+++ source/motion_1/constants.py
@@ -237,3 +237,18 @@ SAMPLING_RANGE = {
     "left_foot_pos": [[-0.0, 0.15, 0.01], [0.0, 0.15, 0.3]],  # Lower limit, upper limit
     "right_foot_pos": [[0.0, -0.15, 0.01], [0.0, -0.15, 0.01]],
 }
+
+END_EFFECTORS = {
+    "l_foot": {
+        "body_name": "left_ankle_roll_link",
+    },
+    "r_foot": {
+        "body_name": "right_ankle_roll_link",
+    },
+    "l_wrist": {
+        "body_name": "left_fist_link",
+    },
+    "r_wrist": {
+        "body_name": "right_fist_link",
+    },
+}
