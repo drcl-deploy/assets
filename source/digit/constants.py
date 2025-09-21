1083fbe01a951b2e386e1be967d3926035d0e87e
lok-i <lokesha1b2c3@gmail.com>
2025-09-20 21:02:43 -0700

    digit ee added

--- source/digit/constants.py
+++ source/digit/constants.py
@@ -8,3 +8,18 @@ FOOT_CONTACT_BODIES = [
 ]
 
 ROOT_LINK_NAME = "torso_base"
+
+END_EFFECTORS = {
+    "l_foot": {
+        "body_name": "left_leg_toe_roll",
+    },
+    "r_foot": {
+        "body_name": "right_leg_toe_roll",
+    },
+    "l_wrist": {
+        "body_name": "left_arm_wrist_yaw",
+    },
+    "r_wrist": {
+        "body_name": "right_arm_wrist_yaw",
+    },
+}
