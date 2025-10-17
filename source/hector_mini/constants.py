e4de953a4766f37a1ef00840e6f2d70da5416f54
Shuang Peng <shuangpe@usc.edu>
2025-10-17 12:04:37 -0700

    MJlab params update (#29)
    * add: tmp 1016
    
    * add: 1016
    
    * add: 1017 move to shared mot params
    
    * add: 1017 del MPCL, change name mot_params->actuator_params, update readme
    
    * fix torso h

--- source/hector_mini/constants.py
+++ source/hector_mini/constants.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+from ..actuator_params import *
+
+NATURAL_FREQ = 10 * 2.0 * 3.1415926535  # 10Hz
+DAMPING_RATIO = 2.0
+
+STIFFNESS_02 = ARMATURE_ROBOSTRIDE02 * NATURAL_FREQ**2
+
+DAMPING_02 = 2.0 * DAMPING_RATIO * ARMATURE_ROBOSTRIDE02 * NATURAL_FREQ
+
 JOINT_NAMES_EXPR = [
     "left_hip_yaw_joint",
     "right_hip_yaw_joint",
@@ -29,63 +39,86 @@ DEFAULT_JOINT_POS = {
 }
 
 STIFFNESS = {
-    "left_hip_yaw_joint": 10.0,
-    "right_hip_yaw_joint": 10.0,
-    "left_hip_roll_joint": 10.0,
-    "right_hip_roll_joint": 10.0,
-    "left_hip_pitch_joint": 10.0,
-    "right_hip_pitch_joint": 10.0,
-    "left_knee_joint": 10.0,
-    "right_knee_joint": 10.0,
-    "left_calf_joint": 10.0,
-    "right_calf_joint": 10.0,
-    "left_ankle_joint": 10.0,
-    "right_ankle_joint": 10.0,
+    "left_hip_yaw_joint": STIFFNESS_02,
+    "right_hip_yaw_joint": STIFFNESS_02,
+    "left_hip_roll_joint": STIFFNESS_02,
+    "right_hip_roll_joint": STIFFNESS_02,
+    "left_hip_pitch_joint": STIFFNESS_02,
+    "right_hip_pitch_joint": STIFFNESS_02,
+    "left_knee_joint": STIFFNESS_02,
+    "right_knee_joint": STIFFNESS_02,
+    "left_calf_joint": STIFFNESS_02,
+    "right_calf_joint": STIFFNESS_02,
+    "left_ankle_joint": STIFFNESS_02,
+    "right_ankle_joint": STIFFNESS_02,
 }
 
 DAMPING = {
-    "left_hip_yaw_joint": 1.0,
-    "right_hip_yaw_joint": 1.0,
-    "left_hip_roll_joint": 1.0,
-    "right_hip_roll_joint": 1.0,
-    "left_hip_pitch_joint": 1.0,
-    "right_hip_pitch_joint": 1.0,
-    "left_knee_joint": 1.0,
-    "right_knee_joint": 1.0,
-    "left_calf_joint": 1.0,
-    "right_calf_joint": 1.0,
-    "left_ankle_joint": 1.0,
-    "right_ankle_joint": 1.0,
+    "left_hip_yaw_joint": DAMPING_02,
+    "right_hip_yaw_joint": DAMPING_02,
+    "left_hip_roll_joint": DAMPING_02,
+    "right_hip_roll_joint": DAMPING_02,
+    "left_hip_pitch_joint": DAMPING_02,
+    "right_hip_pitch_joint": DAMPING_02,
+    "left_knee_joint": DAMPING_02,
+    "right_knee_joint": DAMPING_02,
+    "left_calf_joint": DAMPING_02,
+    "right_calf_joint": DAMPING_02,
+    "left_ankle_joint": DAMPING_02,
+    "right_ankle_joint": DAMPING_02,
 }
 
 EFFORT_LIMIT = {
-    "left_hip_yaw_joint": 17.0,
-    "right_hip_yaw_joint": 17.0,
-    "left_hip_roll_joint": 17.0,
-    "right_hip_roll_joint": 17.0,
-    "left_hip_pitch_joint": 17.0,
-    "right_hip_pitch_joint": 17.0,
-    "left_knee_joint": 17.0,
-    "right_knee_joint": 17.0,
-    "left_calf_joint": 17.0,
-    "right_calf_joint": 17.0,
-    "left_ankle_joint": 17.0,
-    "right_ankle_joint": 17.0,
+    "left_hip_yaw_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "right_hip_yaw_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "left_hip_roll_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "right_hip_roll_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "left_hip_pitch_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "right_hip_pitch_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "left_knee_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "right_knee_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "left_calf_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "right_calf_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "left_ankle_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
+    "right_ankle_joint": ACTUATOR_ROBOSTRIDE02.effort_limit,
 }
 
 VELOCITY_LIMIT = {
-    "left_hip_yaw_joint": 22.0,
-    "right_hip_yaw_joint": 22.0,
-    "left_hip_roll_joint": 22.0,
-    "right_hip_roll_joint": 22.0,
-    "left_hip_pitch_joint": 22.0,
-    "right_hip_pitch_joint": 22.0,
-    "left_knee_joint": 22.0,
-    "right_knee_joint": 22.0,
-    "left_calf_joint": 22.0,
-    "right_calf_joint": 22.0,
-    "left_ankle_joint": 22.0,
-    "right_ankle_joint": 22.0,
+    "left_hip_yaw_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "right_hip_yaw_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "left_hip_roll_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "right_hip_roll_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "left_hip_pitch_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "right_hip_pitch_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "left_knee_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "right_knee_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "left_calf_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "right_calf_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "left_ankle_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+    "right_ankle_joint": ACTUATOR_ROBOSTRIDE02.velocity_limit,
+}
+
+# Robstride 02, TODO: Consider couping on ankle, knee
+ARMATURE = {
+    "left_hip_yaw_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "right_hip_yaw_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "left_hip_roll_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "right_hip_roll_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "left_hip_pitch_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "right_hip_pitch_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "left_knee_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "right_knee_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "left_calf_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "right_calf_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "left_ankle_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+    "right_ankle_joint": ACTUATOR_ROBOSTRIDE02.reflected_inertia,
+}
+
+# 0.25 * effort / stiffness, in the same per-joint dict style
+ACTION_SCALE = {
+    name: 0.25 * EFFORT_LIMIT[name] / STIFFNESS[name]
+    for name in JOINT_NAMES_EXPR
+    if name in EFFORT_LIMIT and name in STIFFNESS and STIFFNESS[name] != 0
 }
 
 BAD_CONTACT_BODIES = [
@@ -102,38 +135,6 @@ FOOT_CONTACT_BODIES = [
     "left_ankle_link",
     "right_ankle_link",
 ]
-
-# Robstride 02, TODO: Consider couping on ankle, knee
-ARMATURE = {
-    "left_hip_yaw_joint": 0.00414,
-    "right_hip_yaw_joint": 0.00414,
-    "left_hip_roll_joint": 0.00414,
-    "right_hip_roll_joint": 0.00414,
-    "left_hip_pitch_joint": 0.00414,
-    "right_hip_pitch_joint": 0.00414,
-    "left_knee_joint": 0.00414,
-    "right_knee_joint": 0.00414,
-    "left_calf_joint": 0.00414,
-    "right_calf_joint": 0.00414,
-    "left_ankle_joint": 0.00414,
-    "right_ankle_joint": 0.00414,
-}
-
-MPCL = [
-    [-1.57, 1.57],  # l_hip_yaw_joint
-    [-1.57, 1.57],  # r_hip_yaw_joint
-    [-1.0, 1.0],  # l_hip_roll_joint
-    [-1.0, 1.0],  # r_hip_roll_joint
-    [-1.0, 2.57],  # l_hip_pitch_joint
-    [-1.0, 2.57],  # r_hip_pitch_joint
-    [0.1, 1.57],  # l_knee_joint
-    [0.1, 1.57],  # r_knee_joint
-    [-1.57, 1.57],  # l_calf_joint
-    [-1.57, 1.57],  # r_calf_joint
-    [-2.0, 2.0],  # l_ankle_joint
-    [-2.0, 2.0],  # r_ankle_joint
-]
-
 
 ROOT_LINK_NAME = "body"
 TRACKED_EE_LINKS = [
