6d2d5beb46ff889cda611bcc86406ad92b4fe361
lok-i <lokesha1b2c3@gmail.com>
2025-10-27 21:31:04 -0700

    bb added

---
+++ source/basketball/basketball.py
@@ -0,0 +1,29 @@
+import isaaclab.sim as sim_utils
+from isaaclab.assets import RigidObjectCfg
+from isaaclab.sim.spawners.materials import PreviewSurfaceCfg, RigidBodyMaterialCfg
+import os
+
+ASSETS_DIR = os.environ.get("SIM_ASSETS_PATH")
+
+
+BASKETBALL_S5_CFG = RigidObjectCfg(
+    prim_path="{ENV_REGEX_NS}/basketball_s5",
+    spawn=sim_utils.SphereCfg(
+        # size=(CUBE_SIZE, CUBE_SIZE, CUBE_HEIGHT),
+        radius=0.1213,  # meters
+        visual_material=PreviewSurfaceCfg(
+            # Cardboard-like color #C19A6C → sRGB ~ (193,154,108)/255
+            diffuse_color=(193/255.0, 154/255.0, 108/255.0),
+            metallic=0.25,
+            roughness=0.6,
+        ),
+        physics_material=RigidBodyMaterialCfg(static_friction=0.3, dynamic_friction=0.3),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(),
+        mass_props=sim_utils.MassPropertiesCfg(mass=1.0),
+        collision_props=sim_utils.CollisionPropertiesCfg(),
+        activate_contact_sensors=False,
+    ),
+    init_state=RigidObjectCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.1213), lin_vel=(0.0, 0.0, 0.0)
+    ),
+)
