e9fe7c2931ed036b9e3880ea59f006c91f1c9e0e
ShravanShenoy4477 <shravan.shenoy111@gmail.com>
2025-11-02 17:40:07 -0800

    Changes to constants.py and go1.xml

--- source/go1/constants.py
+++ source/go1/constants.py
@@ -36,42 +36,42 @@ DEFAULT_JOINT_POS = {
 
 STIFFNESS = {
     # Hip joints (abduction/adduction)
-    "FL_hip_joint": 20.0,
-    "FR_hip_joint": 20.0,
-    "RL_hip_joint": 20.0,
-    "RR_hip_joint": 20.0,
+    "FL_hip_joint": 40.0,
+    "FR_hip_joint": 40.0,
+    "RL_hip_joint": 40.0,
+    "RR_hip_joint": 40.0,
     
     # Thigh joints (hip flexion/extension)
-    "FL_thigh_joint": 20.0,
-    "FR_thigh_joint": 20.0,
-    "RL_thigh_joint": 20.0,
-    "RR_thigh_joint": 20.0,
+    "FL_thigh_joint": 40.0,
+    "FR_thigh_joint": 40.0,
+    "RL_thigh_joint": 40.0,
+    "RR_thigh_joint": 40.0,
     
     # Calf joints (knee)
-    "FL_calf_joint": 20.0,
-    "FR_calf_joint": 20.0,
-    "RL_calf_joint": 20.0,
-    "RR_calf_joint": 20.0,
+    "FL_calf_joint": 50.0,
+    "FR_calf_joint": 50.0,
+    "RL_calf_joint": 50.0,
+    "RR_calf_joint": 50.0,
 }
 
 DAMPING = {
     # Hip joints
-    "FL_hip_joint": 0.5,
-    "FR_hip_joint": 0.5,
-    "RL_hip_joint": 0.5,
-    "RR_hip_joint": 0.5,
+    "FL_hip_joint": 1.0,
+    "FR_hip_joint": 1.0,
+    "RL_hip_joint": 1.0,
+    "RR_hip_joint": 1.0,
     
     # Thigh joints
-    "FL_thigh_joint": 0.5,
-    "FR_thigh_joint": 0.5,
-    "RL_thigh_joint": 0.5,
-    "RR_thigh_joint": 0.5,
+    "FL_thigh_joint": 1.0,
+    "FR_thigh_joint": 1.0,
+    "RL_thigh_joint": 1.0,
+    "RR_thigh_joint": 1.0,
     
     # Calf joints
-    "FL_calf_joint": 0.5,
-    "FR_calf_joint": 0.5,
-    "RL_calf_joint": 0.5,
-    "RR_calf_joint": 0.5,
+    "FL_calf_joint": 1.2,
+    "FR_calf_joint": 1.2,
+    "RL_calf_joint": 1.2,
+    "RR_calf_joint": 1.2,
 }
 
 
