68839bf501d4ac13826e7d90afa8f86f580f39f3
lok-i <lokesha1b2c3@gmail.com>
2026-05-14 14:21:48 -0700

    patched naming

--- source/g1/g1_bm.py
+++ source/g1/g1_bm.py
@@ -176,9 +176,9 @@ G1_BM_HAND_MESH_CFG = G1_BM_CFG.copy()
 G1_BM_HAND_MESH_CFG.spawn.asset_path = os.path.join(ASSETS_DIR, "g1/g1_bm_hand_mesh.urdf")
 G1_BM_HAND_MESH_CFG.spawn.collider_type = "convex_decomposition"
 
-G1_FLAT_0_CFG = G1_BM_CFG.copy()
-G1_FLAT_0_CFG.spawn.asset_path = os.path.join(ASSETS_DIR, "g1/g1_flat_0.urdf")
-G1_FLAT_0_CFG.spawn.collider_type = "convex_decomposition"
+G1_FLAT_0_HAND_CFG = G1_BM_CFG.copy()
+G1_FLAT_0_HAND_CFG.spawn.asset_path = os.path.join(ASSETS_DIR, "g1/g1_flat_0_hand.urdf")
+G1_FLAT_0_HAND_CFG.spawn.collider_type = "convex_decomposition"
 
 G1_BM_ACTION_SCALE = {}
 
